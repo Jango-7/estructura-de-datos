db3304bdb952a23a856b32f0f75e32768b476214
unknown <jhoanngo2@gmail.com>
2025-02-20 18:21:17 -0300

    practicas

---
+++ practica-bucles.py
@@ -0,0 +1,39 @@
+
+# cursos = ["IA", "Big Data", "SQL", "Python", "SEO"]
+# for curso in cursos:
+#   print(curso)
+
+#--------------------------------------------------
+#--------------------------------------------------
+
+# numeros = []
+
+# for n in range(0,20):
+#   numeros.append(n)
+#   print(numeros)
+
+# ----------------------------------------------
+# ----------------------------------------------
+
+'''
+Ejercicio: Contar vocales en una cadena
+
+Escribe un programa que cuente cuántas vocales (a, e, i, o, u) hay en una cadena de texto ingresada por el usuario. Usa un bucle for para recorrer la cadena y verificar cada carácter.
+
+Requisitos:
+- Pide al usuario que ingrese una cadena de texto.
+- Usa un bucle for para recorrer cada carácter de la cadena.
+- Verifica si el carácter es una vocal (minúscula o mayúscula).
+- Mantén un contador para cada vocal.
+- Al final, imprime el total de vocales encontradas.
+'''
+
+texto = input('Ingresa una frase: ').lower()
+contador = 0
+vocals = ["a", "e", "i", "o", "u"]
+
+for letra in texto:
+  if letra == vocals:
+    contador += 1
+
+print(f'Cantidad de vocales: {contador}')
