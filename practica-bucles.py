1410f5adaa3d5c080f06e6f6dc48db5f71267c91
unknown <jhoanngo2@gmail.com>
2025-03-06 18:17:18 -0300

    Practica bucle for 0603

--- practica-bucles.py
+++ practica-bucles.py
@@ -54,5 +54,32 @@ Escribe un programa que solicite al usuario un número entero positivo n. Luego,
 # ----------------------------------------------
 # ----------------------------------------------
 
-'''Escribe un programa que solicite al usuario una palabra (cadena de texto). Luego, el programa debe imprimir cada letra de la palabra junto con su posición (índice) en la palabra.'''
+'''Escribe un programa que solicite al usuario una palabra (cadena de texto). Luego, el programa debe imprimir cada letra de la palabra junto con su posición (índice) en la palabra.
 
+PISTAS:
+* Puedes usar la función range() junto con len() para obtener los índices de la palabra.
+* Recuerda que las cadenas de texto en Python son iterables, por lo que puedes acceder a cada letra usando su índice.'''
+
+# palabra = input('Ingresa una palabra: ')
+
+# for i in range(len(palabra)):
+#     print(f'indice: {i}: {palabra[i]}')
+
+'''Escribe un programa que solicite al usuario un número entero positivo n. Luego, el programa debe calcular la suma de todos los números impares desde 1 hasta n (inclusive) y mostrar el resultado.
+
+Por ejemplo, si el usuario ingresa 10, el programa debería calcular la suma de los números impares 1 + 3 + 5 + 7 + 9 y mostrar:
+
+Pistas:
+* Usa un ciclo for para iterar sobre los números desde 1 hasta n.
+* Usa una variable para acumular la suma de los números impares.
+* Recuerda que un número es impar si el residuo de dividirlo entre 2 no es cero (numero % 2 != 0).'''
+
+
+numero = int(input("Ingresa un numero entero: "))
+suma_impar = 0
+
+for n in range(1, numero + 1):
+    if n % 2 != 0:
+        suma_impar += n
+
+print(suma_impar)
